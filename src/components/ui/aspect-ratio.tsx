324fb1624fd2c5084e6b2485cef2d606e3ce7b85
Avani Garg <142567720+avnigarg12@users.noreply.github.com>
2025-11-29 12:08:44 +0530

    Update aspect-ratio.tsx

--- src/components/ui/aspect-ratio.tsx
+++ src/components/ui/aspect-ratio.tsx
@@ -1,13 +1,12 @@
 "use client";
 
-import * as AspectRatioPrimitive from " 
-    [0]
-  ";
+import * as AspectRatioPrimitive from "@radix-ui/react-aspect-ratio";
 
-function AspectRatio({
-  ...props
-}: React.ComponentProps<typeof AspectRatioPrimitive.Root>) {
+function AspectRatio(
+  props: React.ComponentProps<typeof AspectRatioPrimitive.Root>
+) {
   return <AspectRatioPrimitive.Root data-slot="aspect-ratio" {...props} />;
 }
 
 export { AspectRatio };
+
